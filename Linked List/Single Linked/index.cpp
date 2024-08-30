5eeca6385162e5abdb9af13015c644a40a3e9c9c
Hamza Asghar <hamzaraftar765@gmail.com>
2024-08-30 10:22:39 +0500

    Correct code

--- Linked List/Single Linked/index.cpp
+++ Linked List/Single Linked/index.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
 using namespace std;
-struct Node
-{
-    int data;
-    Node *next;
-};
-struct Node *head = NULL, *temp;
-void insert(int n)
-{
-    Node *newNode = new Node;
-    newNode->data = n;
-    newNode->next = head;
-    head = newNode;
-}
-void prient()
-{
-    cout << "Data Elements in Single Linked List are: ";
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << "  ";
-        temp = temp->next;
-    }
-}
+
 int main()
 {
-    insert(1);
-    insert(2);
-    insert(3);
-    insert(4);
-    insert(5);
-    insert(6);
-    prient();
+    struct node
+    {
+        int data;
+        struct node *next;
+    };
+    struct node *head, *temp;
+    head = 0;
+    int choise;
+    while (choise)
+    {
+
+        node *newnode = new node;
+        cout << "Enter data :" << endl;
+        cin >> newnode->data;
+        newnode->next = 0;
+        if (head == 0)
+        {
+            head = temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+        cout << "Do you want to continue (1,0) ?" << endl;
+        cin >> choise;
+    }
+    temp = head;
+    while (temp != 0)
+    {
+        cout << temp->data << "   ";
+        temp = temp->next;
+    }
     return 0;
 }
