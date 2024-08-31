1161e78366144bf4deb6acfef7d510e699b4d1c6
Hamza Asghar <hamzaraftar765@gmail.com>
2024-08-31 21:17:11 +0500

    insert at begging

---
+++ Linked List/Single Linked/at begging/index.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int pos, i;
+    struct node
+    {
+        int data;
+        struct node *next;
+    };
+    struct node *head, *temp;
+    head = 0;
+    int choise;
+    while (choise)
+    {
+
+        node *newnode = new node;
+        cout << "Enter data :" << endl;
+        cin >> newnode->data;
+        newnode->next = 0;
+        if (head == 0)
+        {
+            head = temp = newnode;
+        }
+        else
+        {
+            temp->next = newnode;
+            temp = newnode;
+        }
+        cout << "Do you want to continue (1,0) ?" << endl;
+        cin >> choise;
+    }
+    temp = head;
+    while (temp != 0)
+    {
+        cout << temp->data << "   " << endl;
+        temp = temp->next;
+    }
+
+    // -- -- -- -- -- -- -inser at begging-- -- -- -- -- --
+
+    node *newnode = new node;
+    cout << "Enter data you want to insert at begging  :" << endl;
+    cin >> newnode->data;
+    newnode->next = head;
+    head = newnode;
+
+    // *****for prient of Linkedlist*****
+    cout << "Linklist after insert at begging" << endl;
+
+    temp = head;
+    while (temp != 0)
+    {
+        cout << temp->data << "   ";
+        temp = temp->next;
+    }
+}
