0189ce785f55af5d940d8eb0de94c4653c980bde
Hamza Asghar <hamzaraftar765@gmail.com>
2024-09-11 12:42:09 +0500

    specific position

---
+++ Linked List/Doubly Linked List/INSERTION/specific position/index.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *pre;
+};
+struct node *head, *tail;
+
+void CreateDLL()
+{
+    int choise;
+    while (choise)
+    {
+        node *newNode = new node;
+        cout << "Enter data " << endl;
+        cin >> newNode->data;
+        newNode->pre = 0;
+        newNode->next = 0;
+        if (head == 0)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->pre = tail;
+            tail = newNode;
+        }
+        cout << "Do you want to continue (1,0) ? " << endl;
+        cin >> choise;
+    }
+}
+
+void InsertAtspecificPosition()
+{
+    int pos, i = 1;
+    cout << "Enter position " << endl;
+    cin >> pos;
+    struct node *temp;
+    temp = head;
+    node *newNode = new node;
+    cout << "Enter data at specific position : " << endl;
+    cin >> newNode->data;
+    newNode->next = 0;
+    newNode->pre = 0;
+    while (i < pos - 1)
+    {
+        temp = temp->next;
+        i++;
+    }
+    newNode->pre = temp;
+    newNode->next = temp->next;
+    temp->next = newNode;
+    newNode->next->pre = newNode;
+}
+
+void Display()
+{
+    cout << "The Elements of Linklsit are : " << endl;
+    struct node *temp;
+    temp = head;
+    while (temp != 0)
+    {
+        cout << temp->data << "   " << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    CreateDLL();
+    Display();
+    InsertAtspecificPosition();
+    Display();
+    return 0;
+}
