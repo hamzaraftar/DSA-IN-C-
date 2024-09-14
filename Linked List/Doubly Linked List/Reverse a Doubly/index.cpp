e5c5e94e6a2c24d19414ae1b73ba5581bc0d7d0d
Hamza Asghar <hamzaraftar765@gmail.com>
2024-09-14 12:47:02 +0500

    Reverse a Doubly

---
+++ Linked List/Doubly Linked List/Reverse a Doubly/index.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *pre;
+};
+struct node *head, *tail;
+
+void CreateDLL()
+{
+    int choise;
+    while (choise)
+    {
+        node *newNode = new node;
+        cout << "Enter data " << endl;
+        cin >> newNode->data;
+        newNode->pre = 0;
+        newNode->next = 0;
+        if (head == 0)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->pre = tail;
+            tail = newNode;
+        }
+        cout << "Do you want to continue (1,0) ? " << endl;
+        cin >> choise;
+    }
+}
+
+void ReverseDll()
+{
+    cout << "After reverse " << endl;
+    struct node *current, *nextNode;
+    current = head;
+    while (current != 0)
+    {
+        nextNode = current->next;
+        current->next = current->pre;
+        current->pre = nextNode;
+        current = nextNode;
+    }
+    current = head;
+    head = tail;
+    tail = current;
+}
+
+void Display()
+{
+    cout << "The Elements of Linklsit are : " << endl;
+    struct node *temp;
+    temp = head;
+    while (temp != 0)
+    {
+        cout << temp->data << "   " << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    CreateDLL();
+    Display();
+    ReverseDll();
+    Display();
+    return 0;
+}
