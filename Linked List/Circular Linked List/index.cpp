88f4961812755ec2792d34e8b8d1e8d68e051fcf
Hamza Asghar <hamzaraftar765@gmail.com>
2024-09-15 12:38:06 +0500

    circular linklist

---
+++ Linked List/Circular Linked List/index.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head;
+void CreateDll()
+{
+    int choise;
+    head = 0;
+    struct node *temp;
+    while (choise)
+    {
+        node *newNode = new node;
+        cout << "Enter data :" << endl;
+        cin >> newNode->data;
+        newNode->next = 0;
+        if (head == 0)
+        {
+            head = temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+        temp->next = head;
+        cout << "Do you want to continue press (1,0) : " << endl;
+        cin >> choise;
+    }
+}
+
+void Display()
+{
+    struct node *temp;
+    temp = head;
+    cout << "The nodes in Linklist are :" << endl;
+    while (temp->next != head)
+    {
+        cout << temp->data << "   " << endl;
+        temp = temp->next;
+    }
+    cout << temp->data << "  " << endl;
+}
+
+int main()
+{
+    CreateDll();
+    Display();
+    return 0;
+}
