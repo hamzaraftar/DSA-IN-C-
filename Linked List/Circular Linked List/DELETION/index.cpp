3acd86605f095449dbe973e96b4d16ae88459e8a
Hamza Asghr <hamzaraftar765@gmail.com>
2024-09-18 22:34:44 -0700

    deletion from beginning

---
+++ Linked List/Circular Linked List/DELETION/index.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *tail;
+void CreateCLL()
+{
+    int choise = 1;
+    tail = 0;
+    while (choise)
+    {
+        node *newNode = new node;
+        cout << "Enter data :" << endl;
+        cin >> newNode->data;
+        newNode->next = 0;
+        if (tail == 0)
+        {
+            tail = newNode;
+            tail->next = newNode;
+        }
+        else
+        {
+            newNode->next = tail->next;
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cout << "Do you want to continue press (1,0) " << endl;
+        cin >> choise;
+    }
+}
+
+void DeletionAtBeginning()
+{
+    cout << "After deletion at beginning :" << endl;
+    struct node *temp;
+    temp = tail->next;
+    if (tail == 0)
+    {
+        cout << "List is empty :" << endl;
+    }
+    else
+    {
+        tail->next = temp->next;
+        delete (temp);
+    }
+}
+
+void DisplayCLL()
+{
+    cout << "The nodes in linklist are :" << endl;
+    struct node *temp;
+    if (tail == 0)
+    {
+        cout << "List is Empty :" << endl;
+    }
+    else
+    {
+        temp = tail->next;
+        while (temp->next != tail->next)
+        {
+            cout << temp->data << "   " << endl;
+            temp = temp->next;
+        }
+        cout << temp->data << "   " << endl;
+    }
+}
+
+int main()
+{
+    CreateCLL();
+    DisplayCLL();
+    DeletionAtBeginning();
+    DisplayCLL();
+    return 0;
+}
