7e9d3a95cdfefda7c23f9f7820930b2c52b6b0b5
Hamza Asghr <hamzaraftar765@gmail.com>
2024-09-18 23:19:48 -0700

    deletion from end

---
+++ Linked List/Circular Linked List/DELETION/deletion from end/index.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *tail;
+void CreateCLL()
+{
+    int choise = 1;
+    tail = 0;
+    while (choise)
+    {
+        node *newNode = new node;
+        cout << "Enter data :" << endl;
+        cin >> newNode->data;
+        newNode->next = 0;
+        if (tail == 0)
+        {
+            tail = newNode;
+            tail->next = newNode;
+        }
+        else
+        {
+            newNode->next = tail->next;
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cout << "Do you want to continue press (1,0) " << endl;
+        cin >> choise;
+    }
+}
+
+void DeletionFromEnd()
+{
+    cout << "After deletion from end :" << endl;
+    struct node *current, *previous;
+    current = tail->next;
+    if (tail == 0)
+    {
+        cout << "List is Empty :" << endl;
+    }
+    else
+    {
+        while (current->next != tail->next)
+        {
+            previous = current;
+            current = current->next;
+        }
+        previous->next = tail->next;
+        tail = previous;
+        delete (current);
+    }
+}
+
+void DisplayCLL()
+{
+    cout << "The nodes in linklist are :" << endl;
+    struct node *temp;
+    if (tail == 0)
+    {
+        cout << "List is Empty :" << endl;
+    }
+    else
+    {
+        temp = tail->next;
+        while (temp->next != tail->next)
+        {
+            cout << temp->data << "   " << endl;
+            temp = temp->next;
+        }
+        cout << temp->data << "   " << endl;
+    }
+}
+
+int main()
+{
+    CreateCLL();
+    DisplayCLL();
+    DeletionFromEnd();
+    DisplayCLL();
+    return 0;
+}
