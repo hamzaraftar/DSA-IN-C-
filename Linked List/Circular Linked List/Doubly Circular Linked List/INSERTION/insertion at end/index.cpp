ce2aec41b224db64cbafe3e9f80660a499a7577b
Hamza Asghar <hamzaraftar765@gmail.com>
2024-09-22 09:14:56 +0500

    insertion at end in DCLL

---
+++ Linked List/Circular Linked List/Doubly Circular Linked List/INSERTION/insertion at end/index.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+struct node *head, *tail;
+
+void CreateDCLL()
+{
+    int choise;
+    head = 0;
+    while (choise)
+    {
+        node *newNode = new node;
+        cout << "Enter data :" << endl;
+        cin >> newNode->data;
+        if (head == 0)
+        {
+            head = tail = newNode;
+            head->next = head;
+            head->prev = head;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            newNode->next = head;
+            head->prev = newNode;
+            tail = newNode;
+        }
+        cout << "Do you want to continue press(0,1)" << endl;
+        cin >> choise;
+    }
+}
+
+void insertionAtEnd()
+{
+    cout << "Insertion at End :" << endl;
+    node *newNode = new node;
+    cout << "Enter data " << endl;
+    cin >> newNode->data;
+    newNode->prev = tail;
+    tail->next = newNode;
+    newNode->next = head;
+    head->prev = newNode;
+    tail = newNode;
+}
+
+void Display()
+{
+    cout << "The node in Circular Linklist are :" << endl;
+    struct node *temp;
+    temp = head;
+    if (head == 0)
+    {
+        cout << "List is Empty :" << endl;
+    }
+    else
+    {
+        while (temp != tail)
+        {
+            cout << temp->data << " " << endl;
+            temp = temp->next;
+        }
+        cout << temp->data << " " << endl;
+    }
+}
+
+int main()
+{
+    CreateDCLL();
+    Display();
+    insertionAtEnd();
+    Display();
+    return 0;
+}
