a8ba27ac1d030467ee98dd834b438089f09e5f1a
Hamza Asghar <hamzaraftar765@gmail.com>
2024-09-22 09:35:53 +0500

    DCC insert at sep pos

---
+++ Linked List/Circular Linked List/Doubly Circular Linked List/INSERTION/insertion at sep pos/index.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+struct node *head, *tail;
+
+void CreateDCLL()
+{
+    int choise;
+    head = 0;
+    while (choise)
+    {
+        node *newNode = new node;
+        cout << "Enter data :" << endl;
+        cin >> newNode->data;
+        if (head == 0)
+        {
+            head = tail = newNode;
+            head->next = head;
+            head->prev = head;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            newNode->next = head;
+            head->prev = newNode;
+            tail = newNode;
+        }
+        cout << "Do you want to continue press(0,1)" << endl;
+        cin >> choise;
+    }
+}
+
+void insertionAtSepPos()
+{
+    int pos, i = 1;
+    struct node *temp;
+    temp = head;
+    cout << "Enter position" << endl;
+    cin >> pos;
+    node *newNode = new node;
+    cout << "Enter data :" << endl;
+    cin >> newNode->data;
+    while (i < pos - 1)
+    {
+        temp = temp->next;
+        i++;
+    }
+    newNode->prev = temp;
+    newNode->next = temp->next;
+    temp->next->prev = newNode;
+    temp->next = newNode;
+}
+
+void Display()
+{
+    cout << "The node in Circular Linklist are :" << endl;
+    struct node *temp;
+    temp = head;
+    if (head == 0)
+    {
+        cout << "List is Empty :" << endl;
+    }
+    else
+    {
+        while (temp != tail)
+        {
+            cout << temp->data << " " << endl;
+            temp = temp->next;
+        }
+        cout << temp->data << " " << endl;
+    }
+}
+
+int main()
+{
+    CreateDCLL();
+    Display();
+    insertionAtSepPos();
+    Display();
+    return 0;
+}
