c05dc61d682a2403db14cbf5842a3ac49a9de3a0
Hamza Asghar <hamzaraftar765@gmail.com>
2024-09-23 08:36:45 +0500

    delete at end

---
+++ Linked List/Circular Linked List/Doubly Circular Linked List/DELETION/delete at end/index.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+struct node *head, *tail;
+
+void CreateDCLL()
+{
+    int choise;
+    head = 0;
+    while (choise)
+    {
+        node *newNode = new node;
+        cout << "Enter data :" << endl;
+        cin >> newNode->data;
+        if (head == 0)
+        {
+            head = tail = newNode;
+            head->next = head;
+            head->prev = head;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            newNode->next = head;
+            head->prev = newNode;
+            tail = newNode;
+        }
+        cout << "Do you want to continue press(0,1)" << endl;
+        cin >> choise;
+    }
+}
+
+void DeleteAtEnd()
+{
+    cout << "After Delete at End :" << endl;
+    struct node *temp;
+    temp = tail;
+    tail = tail->prev;
+    tail->next = head;
+    head->prev = tail;
+    delete (temp);
+}
+void Display()
+{
+    cout << "The node in Circular Linklist are :" << endl;
+    struct node *temp;
+    temp = head;
+    if (head == 0)
+    {
+        cout << "List is Empty :" << endl;
+    }
+    else
+    {
+        while (temp != tail)
+        {
+            cout << temp->data << " " << endl;
+            temp = temp->next;
+        }
+        cout << temp->data << " " << endl;
+    }
+}
+
+int main()
+{
+    CreateDCLL();
+    Display();
+    DeleteAtEnd();
+    Display();
+    return 0;
+}
