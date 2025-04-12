533b337568b0fe364874073945616d20b76dc22d
Hamza Asghar <hamzaraftar765@gmail.com>
2025-04-12 19:46:00 +0500

    linklist using classes

---
+++ Linked List/Single Linklist/using_c++/list.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include "Node.cpp"
+using namespace std;
+
+class List
+{
+private:
+    Node *headNode;
+    Node *currentNode;
+    Node *lastCurrentNode;
+    int size;
+
+public:
+    // Constructor
+    List()
+    {
+        headNode = new Node();
+        headNode->setNext(NULL);
+        currentNode = NULL;
+        size = 0;
+    };
+    void add(int addObject)
+    {
+        Node *newNode = new Node();
+        newNode->set(addObject);
+        newNode->setNext(NULL);
+
+        if (currentNode != NULL)
+        {
+            newNode->setNext(currentNode->getNext());
+            currentNode->setNext(newNode);
+            lastCurrentNode = currentNode;
+            currentNode = newNode;
+        }
+        else
+        {
+            newNode->setNext(NULL);
+            headNode->setNext(newNode);
+            lastCurrentNode = headNode;
+            currentNode = newNode;
+        }
+        size++;
+    };
+    int get()
+    {
+        if (currentNode != NULL)
+        {
+            return currentNode->get();
+        }
+    }
+    void printList()
+    {
+        Node *temp = headNode->getNext(); // skip dummy node
+        while (temp != NULL)
+        {
+            cout << temp->get() << " ";
+            temp = temp->getNext();
+        }
+        // cout << "NULL" << endl;
+    }
+};
+
+int main()
+{
+    List list;
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.printList();
+
+    return 0;
+}
